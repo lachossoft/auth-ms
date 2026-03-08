fdb7fa3a534ce717d084a7aeff357c6494a91825
Horacio <151089759+devmfcancun@users.noreply.github.com>
2026-03-07 23:15:41 -0500

    Add configurador de variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
+  await app.listen(envs.port, () => {
+    logger.log(`Server is running on port ${envs.port}`);
+  });
 }
+
 bootstrap();
